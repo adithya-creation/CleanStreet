04bde808f294c275e187e7b6183b9a0353b86711
Kaviya Lakshminarayanan <kaviyal292@gmail.com>
2026-02-17 08:28:13 +0530

    Footer Updated

--- clean-street/src/App.jsx
+++ clean-street/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
-import Dashboard from './Components/Dashboard/Dashboard'
+import Dashboard from './Components/Dashboard'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
 
 
 function App() {
   return (
     <>
+    <Navbar/>
     <Dashboard/>
+    <Footer/>
     </>
   )
 }
--- clean-street/src/Components/Dashboard.jsx
+++ clean-street/src/Components/Dashboard.jsx
@@ -19,38 +19,7 @@ const Dashboard = () => {
 
   return (
     <div className="hero min-h-screen w-full relative overflow-x-hidden font-sans bg-gradient-to-b from-[#FFF6F0] to-[#E2F5F2]">
-      <motion.nav 
-        initial={{ y: -50, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.6 }}
-        className="relative z-20 bg-white flex justify-between p-3 items-center  md:px-16 w-full"
-      >
-        <div className="text-2xl font-bold text-teal-600 tracking-wide cursor-pointer">
-          CLEAN STREET
-        </div>
-
-        <div className="hidden md:flex space-x-8 text-sm font-semibold text-gray-500">
-         
-            <a href="#" className="hover:text-teal-600 transition-colors relative group">HOME</a>
-            <a href="#" className="hover:text-teal-600 transition-colors relative group">ABOUT</a>
-            <a href="#" className="hover:text-teal-600 transition-colors relative group">REPORT ISSUE</a>
-            <a href="#" className="hover:text-teal-600 transition-colors relative group">VIEW COMPLAINT</a>
-
-              
-        </div>
-
-        <motion.div 
-          whileHover={{ scale: 1.05 }}
-          className="flex items-center space-x-3 cursor-pointer group"
-        >
-          <span className="font-semibold text-teal-700 hidden sm:block uppercase group-hover:text-teal-900">
-            USER NAME
-          </span>
-          <div className="w-10 h-10 rounded-full bg-teal-100 flex items-center justify-center text-teal-700 shadow-sm border border-teal-200 group-hover:bg-teal-200 transition">
-            <FontAwesomeIcon icon={faUser} />
-          </div>
-        </motion.div>
-      </motion.nav>
+      
       <main className="relative z-10 flex flex-col items-center justify-center px-6 mt-10 text-center">
         
         <motion.div
