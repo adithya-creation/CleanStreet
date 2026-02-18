f7e81acbdf4d0970026978b677c828534f55ffb4
adithya-creation <adithyamittapelli888@gmail.com>
2026-02-18 19:04:07 +0530

    update on user-management

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,23 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './Pages/NavBar'
-import Dashboard from './Pages/Dashboard'
-import Footer from './Pages/Footer'
-import Register from "./pages/Register";
+import './App.css';
+import Navbar from './Pages/NavBar';
+import Dashboard from './Pages/Dashboard';
+import Footer from './Pages/Footer';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <Navbar/>
-      <Dashboard/>
-      <Footer/>
-    </>
-  )
-
-function App() {
-  return <Register />;
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -1,7 +1,8 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import { motion } from 'framer-motion';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser,  faMapLocationDot,  faCity,  faChartLine,  faComments, faPlus, faEye } from '@fortawesome/free-solid-svg-icons';
+import { faMapLocationDot, faCity, faChartLine, faComments, faPlus, faEye } from '@fortawesome/free-solid-svg-icons';
 
 const Dashboard = () => {
   const containerVariants = {
