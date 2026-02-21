39eec6bcc9364e89bd9836561bf3bfca6a704522
akarshto <akarsh1064@gmail.com>
2026-02-21 19:05:45 +0530

    Backend connected to MongoDB Atlas

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
 
+require("dotenv").config();
+const connectDB = require("../config/db");
+
 const app = express();
+
+// CONNECT DATABASE
+connectDB();
 
 app.use(cors());
 app.use(express.json());
@@ -14,4 +20,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Backend running on port ${PORT}`);
 });
-
