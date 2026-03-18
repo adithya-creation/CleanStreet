663b9f919aea179aa485730a2b4f1bb29d5d3cbe
Adelinespnraj24 <adelinespnraj@gmail.com>
2026-03-18 19:27:22 +0530

    user feedback frontend

--- frontend/src/services/feedbackService.js
+++ frontend/src/services/feedbackService.js
@@ -1,6 +1,31 @@
-import axios from "axios";
+import api from "./api"; 
 
+// 1. ADD THIS: This is what Feedback.jsx is looking for!
 export const submitFeedback = async (feedbackData) => {
-  const res = await axios.post("/api/feedback", feedbackData);
-  return res.data;
+    try {
+        // feedbackData should be { type, rating, comment, complaintId }
+        const response = await api.post("/feedback/submit", feedbackData);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+// 2. Keep your existing Admin functions below
+export const getAllFeedbacks = async () => {
+    try {
+        const response = await api.get("/feedback/all");
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const replyToFeedback = async (feedbackId, adminReply) => {
+    try {
+        const response = await api.post(`/feedback/reply/${feedbackId}`, { adminReply });
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
 };
